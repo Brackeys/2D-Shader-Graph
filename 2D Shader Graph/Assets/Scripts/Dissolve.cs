87dce1085174316439d7f99f7c2a5e7c3120cbaf
Asbjørn Thirslund <at@brackeys.com>
2020-02-11 11:40:35 +0100

    Initial commit

---
+++ 2D Shader Graph/Assets/Scripts/Dissolve.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+	Material material;
+
+	bool isDissolving = false;
+	float fade = 1f;
+
+	void Start()
+	{
+		// Get a reference to the material
+		material = GetComponent<SpriteRenderer>().material;
+	}
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+			isDissolving = true;
+		}
+
+		if (isDissolving)
+		{
+			fade -= Time.deltaTime;
+
+			if (fade <= 0f)
+			{
+				fade = 0f;
+				isDissolving = false;
+			}
+
+			// Set the property
+			material.SetFloat("_Fade", fade);
+		}
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# 2D-Shader-Graph
+# 2D Shader Graph
 Project files for our tutorial on how to get started using Shader Graph for 2D by creating a dissolve effect.
+
+
+
+Check out our [YouTube Channel](http://youtube.com/brackeys) for more tutorials.
